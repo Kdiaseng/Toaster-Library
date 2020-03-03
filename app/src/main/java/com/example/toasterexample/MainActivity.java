dea6367fb9f8c7a2b0840ab289333650e9fd48c5
Kaleb Dias da Silva <kaleb.silva@ITBL00110.itbam.org.br>
2020-03-03 08:49:07 -0400

    added method to custom Toast

--- app/src/main/java/com/example/toasterexample/MainActivity.java
+++ app/src/main/java/com/example/toasterexample/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.toasterexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.toasterlibrary.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnToastStart = findViewById(R.id.btnToastStart);
+        Button btnToastCenterV = findViewById(R.id.btnToastCenterVertical);
+        Button btnToastCenter = findViewById(R.id.btnToastCenter);
+        Button btnToastCustom = findViewById(R.id.btnToastCustom);
+
+
+
+        btnToastStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToasterMessage.s(getApplicationContext(), "Library is Working - Toast START", Gravity.START);
+            }
+        });
+
+
+        btnToastCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToasterMessage.s(getApplicationContext(), "Library is Working - Toast DEFAULT");
+            }
+        });
+
+
+        btnToastCenterV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToasterMessage.s(getApplicationContext(), "Library is Working - Toast Vertical", Gravity.CENTER_VERTICAL);
+            }
+        });
+
+        btnToastCustom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               ToasterMessage toasterMessage = new ToasterMessage();
+               toasterMessage.showCustom(getApplicationContext(), MainActivity.this,"Library is working - Toast Custom");
+            }
+        });
+
+        ToasterMessage.s(this, "Library is Working");
     }
 }
--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -1,10 +1,39 @@
 package com.example.toasterlibrary;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class ToasterMessage {
     public static void s(Context c, String message){
-        Toast.makeText(c, message,Toast.LENGTH_LONG).show();
+        Toast.makeText(c, message,Toast.LENGTH_SHORT).show();
     }
+
+    public static void s(Context c, String message, Integer position){
+        Toast toast = Toast.makeText(c, message,Toast.LENGTH_SHORT);
+        toast.setGravity(position,0,0);
+        toast.show();
+    }
+
+    public void showCustom(Context c, Activity activity, String message){
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) activity.findViewById(R.id.custom_toast_container));
+        TextView tv = layout.findViewById(R.id.txtvw);
+
+        tv.setText(message);
+        Toast toast = new Toast(c);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 100);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+
+    }
+
+
 }
